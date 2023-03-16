0c0110bedfb0b71efaba1e01005f9e99760ea8f4
Elvis Otieno <elvisotienomboya@gmail.com>
2023-03-16 04:30:58 -0700

    the firt C code have ever wrote

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+/**
+ * main - the main
+ * return - 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	
+	return 0;
+}
