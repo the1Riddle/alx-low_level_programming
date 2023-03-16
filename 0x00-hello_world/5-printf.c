b9fb4b48183e51ea7976fee50c05edd5aa61e493
Elvis Otieno <elvisotienomboya@gmail.com>
2023-03-16 08:47:03 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: usualy 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
