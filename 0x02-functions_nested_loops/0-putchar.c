de210f3186a8835bdcb7f12a4a4f0ab98caf4e92
Elvis Otieno <elvisotienomboya@gmail.com>
2023-03-22 00:53:08 -0700

    my commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - The entry point
+ * Description: prints _putchar followed by a new line
+ * Return: usually 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+C - Functions and nested loops.
