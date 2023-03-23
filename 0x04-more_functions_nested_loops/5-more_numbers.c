3d60dbce002a3891c1f3bc58cae1e5d555bee774
Elvis Otieno <elvisotienomboya@gmail.com>
2023-03-23 11:00:12 -0700

    a program that  prints 10 times the numbers, from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - The entry point
+ * Description:  prints 10 times the numbers, from 0 to 14
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
