9af92146eb8f89dd42eb73e09b88c53cae779431
Elvis Otieno <elvisotienomboya@gmail.com>
2023-03-23 00:54:45 -0700

    my third commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -6,28 +6,29 @@
  * @b: second integer
  * @c: third integer
  * Return: largest number
-*/
+ */
 
-nt largest_number(int a, int b, int c)
+int largest_number(int a, int b, int c)
+{
+ int largest;
 
-int largest;
-
-if (a > b && a > c)
-{
-largest = a;
-}
-else if (a > b && c > a)
-{
-largest = c;
-}
-else if (b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
+ if (a > b && a > c)
+ {
+ largest = a;
+ }
+ else if (a > b && c > a)
+ {
+ largest = c;
+ }
+ else if (b > c)
+ {
+ largest = b;
+ }
+ else
+ {
+ largest = c;
+ }
 
 return (largest);
 }
+
