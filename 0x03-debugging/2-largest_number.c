6662175747fc4e26ba80803228c1ab4990549b92
Elvis Otieno <elvisotienomboya@gmail.com>
2023-03-23 00:57:56 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,24 +10,24 @@
 
 int largest_number(int a, int b, int c)
 {
- int largest;
+int largest;
 
- if (a > b && a > c)
- {
- largest = a;
- }
- else if (a > b && c > a)
- {
- largest = c;
- }
- else if (b > c)
- {
- largest = b;
- }
- else
- {
- largest = c;
- }
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (a > b && c > a)
+{
+largest = c;
+}
+else if (b > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
 
 return (largest);
 }
