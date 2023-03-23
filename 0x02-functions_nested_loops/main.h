48aa0b99f776282e738833f8c4841041d3806f5b
Elvis Otieno <elvisotienomboya@gmail.com>
2023-03-23 02:01:50 -0700

    my header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,5 +7,11 @@ void jack_bauer(void);
 void times_table(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
 
 #endif
