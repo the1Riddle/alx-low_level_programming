c64f0eb2ae23cb67481b6bd588ceac87753a9c17
Elvis Otieno <elvisotienomboya@gmail.com>
2023-03-29 01:19:13 -0700

    my main header file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 char *_strcat(char *dest, char *src);
+int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
