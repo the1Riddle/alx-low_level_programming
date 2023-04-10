8c63ef7239352b0ae61ca90debced040aaccd2be
Elvis Otieno <elvisotienomboya@gmail.com>
2023-04-10 07:55:53 -0700

    The main header file

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,10 +2,14 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int main(int argc, char *argv[]);
 int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+
 int main(int argc, char *argv[]);
 
 #endif
