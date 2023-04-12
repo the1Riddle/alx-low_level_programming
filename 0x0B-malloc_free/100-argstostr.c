ad4f3c80230d3aa81eda5e3f7c73d7cdd705eb8e
Elvis Otieno <elvisotienomboya@gmail.com>
2023-04-12 08:21:54 -0700

    a function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,8 +1,47 @@
 #include "main.h"
 /**
- * strtow - Split a string into words
- * @str: String to split
+ * argstostr- the entry point
+ * @ac: an int input
+ * @av: pointer to a pointer array
  *
- * Return: Pointer to array of strings (words)
- * or NULL fo failure
+ * Description: a function that concatenates all the arguments of your program
+ * Return: Pointer to string or NULL for failure
  */
+char *argstostr(int ac, char **av)
+{
+	int a, b, c = 0, d = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b]; b++)
+		{
+			d++;
+		}
+	}
+	d += ac;
+
+	str = malloc(sizeof(char) * d + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b]; b++)
+		{
+			str[c] = av[a][b];
+			c++;
+		}
+		if (str[c] == '\0')
+		{
+			str[c++] = '\n';
+		}
+	}
+	return (str);
+}
+
