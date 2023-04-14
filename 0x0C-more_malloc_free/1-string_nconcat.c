e62023ab3d5839eea7acf7a800bceb4ae81b1d8e
Elvis Otieno <elvisotienomboya@gmail.com>
2023-04-14 04:56:44 -0700

    a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,31 +10,42 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	size_t len1 = strlen(s1);
-	size_t len2 = strlen(s2);
+	unsigned int a = 0, b = 0, len1 = 0, len2 = 0;
 
+	while (s1 && s1[len1])
+	{
+		len1++;
+	}
+	while (s2 && s2[len2])
+	{
+		len2++;
+	}
 	if (n < len2)
 	{
-	s = malloc(sizeof(char) * (len1 + n + 1));
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	}
 	else
 	{
-	s = malloc(sizeof(char) * (len1 + len2 + 1));
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 	}
-
 	if (!s)
 	{
-	return (NULL);
+		return (NULL);
 	}
+	while (a < len1)
+	{
+		s[a] = s1[a];
+		a++;
+	}
+	while (n < len2 && a < (len1 + n))
+	{
+		s[a++] = s2[b++];
+	}
+	while (n >= len2 && a < (len1 + len2))
+	{
+		s[a++] = s2[b++];
+	}
+	s[a] = '\0';
 
-	memcpy(s, s1, len1);
-	if (n >= len2)
-	{
-	memcpy(s + len1, s2, len2 + 1);
-	}
-	else
-	{
-	memcpy(s + len1, s2, n);
-	}
 	return (s);
 }
