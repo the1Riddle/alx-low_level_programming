68fd4ca56c9264c6bcfddeb51600a01453122669
Elvis Otieno <elvisotienomboya@gmail.com>
2023-04-19 07:15:07 -0700

    function that selects the correct function to perform the operation asked by the user.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,10 +20,10 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i]->op != NULL && *(ops[i]->op) != *s)
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	{
 		i++;
 	}
 
-	return (ops[i]->f);
+	return (ops[i].f);
 }
