0f5adf1cf9bd591f955d328b3d231da0678f3a17
Elvis Otieno <elvisotienomboya@gmail.com>
2023-04-19 07:03:19 -0700

    a program that uses all the arithmetic operators

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - Returns the sum of two numbers
+ * @a: variable1
+ * @b: variable2
+ *
+ * Return: The sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Returns the difference of two numbers
+ * @a: var1
+ * @b: var2
+ *
+ * Return: The difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Returns the product of two numbers
+ * @a: var1
+ * @b: var2
+ *
+ * Return: The product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Returns the division of two numbers
+ * @a: var1
+ * @b: var2
+ *
+ * Return: The quotient of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Returns the remainder of the division of two numbers
+ * @a: var1
+ * @b: var2
+ *
+ * Return: The remainder of the division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
