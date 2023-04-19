ed9b7617fecaf1bea32e18c4fef8d1164efd2288
Elvis Otieno <elvisotienomboya@gmail.com>
2023-04-19 05:38:37 -0700

    function returns a pointer to the function that corresponds to the operator given as a parameter.

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+/**
+ * get_op_func - the entry point
+ * @s: The operator passed as argument.
+ *
+ * Description: This function returns a pointer to the function,
+ * that corresponds to the operator given as a parameter
+ *
+ * Return: NULL if s does not match of +, -, *, /, % 
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+	int i = 0;
+
+	while (ops[i]->op != NULL && *(ops[i]->op) != *s)
+	{
+		i++;
+	}
+
+	return (ops[i]->f);
+}
