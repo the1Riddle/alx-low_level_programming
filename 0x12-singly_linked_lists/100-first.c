7caa613281f12ad72fa013334c805f57ccd0dec9
ElvisMalsa <elvisotienomboya@gmail.com>
2023-04-27 07:33:39 -0700

    function that prints before the main function is executed.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,20 @@
+#include ""
+
+/**
+ * first - my proto
+ *
+ * Description: it aplys the constructor attribute to the first()
+ * so that it is executed before main()
+ */
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - the entryb point
+ *
+ * Description: prints a sentence before the main function is executed
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
