795fc734f8e6f036eaa2befe98186f108cfa85a3
ElvisMalsa <elvisotienomboya@gmail.com>
2023-05-02 06:42:24 -0700

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * free_listint2 - frees list
+ * @head: pointer to listint_t
+ *
+ * Description: the unction sets the head to NULL
+ * and free's listint_t
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *frii;
+
+	while (head == NULL)
+	{
+		frii = head->next;
+		free(head);
+		head = frii;
+	}
+}
