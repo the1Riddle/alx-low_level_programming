add2030c41f16f2da2d9d3bb57be426358a329a8
ElvisMalsa <elvisotienomboya@gmail.com>
2023-05-08 09:38:12 -0700

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,13 +13,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		return -1;
+		return (-1);
 	}
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 	{
-		return -1;
+		return (-1);
 	}
 
 	if (text_content != NULL)
@@ -27,11 +27,11 @@ int create_file(const char *filename, char *text_content)
 		bytesW = write(fd, text_content, strlen(text_content));
 		if (bytesW == -1)
 		{
-			close(fd);  
-			return -1;
-		}       
-	}   
+			close(fd);
+			return (-1);
+		}
+	}
 
 	close(fd);
-	return 1;
+	return (1);
 }
