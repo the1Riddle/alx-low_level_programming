441b04eb43931a246e1c58a98d3ffb95b2c6e96f
ElvisMalsa <elvisotienomboya@gmail.com>
2023-05-08 08:58:50 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,29 +13,29 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file;
+	int fd;
 	char *buffer;
-	ssize_t bytesR;
+	ssize_t bytesR, bytesW;
 
 	if (filename == NULL)
 		return (0);
 
-	file = fopen(filename, "r");
-	if (file == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		fclose(file);
+		close(fd);
 		return (0);
 	}
 
-	bytesR = fread(buffer, sizeof(char), letters, file);
-	fwrite(buffer, sizeof(char), bytesR, stdout);
+	bytesR = read(fd, buffer, letters);
+	bytesW = write(STDOUT_FILENO, buffer, bytesR);
 
 	free(buffer);
-	fclose(file);
+	close(fd);
 
-	return (bytesR);
+	return (bytesW);
 }
