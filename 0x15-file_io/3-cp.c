42bcd72c117652f52d0cf3addc93336690501b95
ElvisMalsa <elvisotienomboya@gmail.com>
2023-05-11 10:13:56 -0700

    performs cp func

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,81 @@
+#include "main.h"
+
+#define BUFF_SIZE 1024
+
+void close_file(int fd);
+/**
+ * main - the entry point
+ * @argv: pointer to the arguments
+ * @argc: number of arguments passed
+ *
+ * Description: this is program that copies the content of a file to another
+ * file.
+ * if the number of argumments are not the same then it exits with code
+ * 97.
+ * if the file exists then it's trancated to the file being copied to that
+ * is file_to.
+ * if the file to be copied from does not exist or cannot be read
+ * then it exits with code 98.
+ * if the file_to cannot be written on or created then it exits with code 99.
+ * if the file descriptor cant be closed then is
+ * exited with code 100 and finally it exits with code 1 if the contents read
+ * is not the same as the contents writen.
+ *
+ * Return: 0 for main funcs (success)
+ */
+int main(int argc, char *argv[])
+{
+	ssize_t bytesW, bytesR;
+	int file_from, file_to;
+	char buff[BUFF_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while ((bytesR = read(file_from, buff, BUFF_SIZE)) > 0)
+	{
+		bytesW = write(file_to, buff, bytesR);
+		if (bytesW != bytesR)
+		{
+			dprintf(STDERR_FILENO, "%s\n", argv[2]);
+			exit(1);
+		}
+	}
+
+	close_file(file_from);
+	close_file(file_to);
+
+	return (0);
+}
+/**
+ * close_file - a func that closes file
+ * @fd: the file descripter
+ *
+ */
+void close_file(int fd)
+{
+	int result = close(fd);
+
+	if (result == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
