7e5a9f56ba938d79e6cdf60e08e64a37a04ef4ad
ElvisMalsa <elvisotienomboya@gmail.com>
2023-05-12 12:16:01 -0700

    appends text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,6 +15,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t bytesW;
 
+	if (text_content == NULL)
+	{
+		return (-1);
+	}
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 	{
