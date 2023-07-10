93fd3694ce18e22c06a5643ce44b9a69c1a29d6d
elvismalsa <elvisotienomboya@gmail.com>
2023-07-10 09:59:47 +0300

    the main header file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
+int main(int argc, char *argv[]);
+
+#endif
