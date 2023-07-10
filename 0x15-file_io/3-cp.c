6fd0c12a0de7ba771b54250f242fadea20e958a3
elvismalsa <elvisotienomboya@gmail.com>
2023-07-10 12:37:32 -0700

    performs ctrl-c ctrl-v on files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -40,33 +40,28 @@ int main(int argc, char *argv[])
 	ssize_t bytesW, bytesR;
 	int file_from, file_to;
 	char buff[BUFF_SIZE];
-	int x = 0;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+
 	file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	while (x <= buff)
+	while ((bytesR = read(file_from, buff, BUFF_SIZE)) > 0)
 	{
-		bytesR = read(file_from, buff, BUFF_SIZE);
-		if (bytesR == 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
 		bytesW = write(file_to, buff, bytesR);
 		if (bytesW != bytesR)
 		{
@@ -76,5 +71,6 @@ int main(int argc, char *argv[])
 	}
 	close_file(file_from);
 	close_file(file_to);
+
 	return (0);
 }
