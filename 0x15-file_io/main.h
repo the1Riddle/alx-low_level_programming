4ce2a0d7112df1bac2fa0d7c647089d2b6dbc793
elvismalsa <elvisotienomboya@gmail.com>
2023-07-14 13:09:19 -0700

    the main header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <elf.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,5 +17,8 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
 int main(int argc, char *argv[]);
+void check_elf(unsigned char *e_ident);
+void print_info(unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif
