91085cec51f0bf464b4b28f8329383317cf2bb16
elvismalsa <elvisotienomboya@gmail.com>
2023-08-17 08:00:47 -0700

    Free list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -9,10 +9,11 @@ void free_dlistint(dlistint_t *head);
 {
 	if (head == NULL)
 		return;
+
 	while (head->next)
 	{
 		head = head->next;
-		free(head->prev)
+		free(head->prev);
 	}
 	free(head);
 }
