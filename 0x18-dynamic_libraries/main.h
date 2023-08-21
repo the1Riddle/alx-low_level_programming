80e8bdf46283715a5443a26d6ede3d819c87bdff
elvismalsa <elvisotienomboya@gmail.com>
2023-08-21 03:23:02 -0700

    the main header file

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -24,5 +23,10 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int add(int x, int y);
+int sub(int x, int y);
+int div(int x, int y);
+int mod(int x, int y);
+int mul(int x, int y);
 
 #endif /* ends the file */
