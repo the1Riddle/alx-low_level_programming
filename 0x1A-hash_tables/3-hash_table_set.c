6ae48a045992ce5b2bf21759d19d93254748204a
elvismalsa <elvisotienomboya@gmail.com>
2023-09-08 08:23:58 -0700

    a function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,8 +17,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !key || *key == '\0' || !value)
 		return (0);
 
-	value_copy = strdup(value);
-	if (value_copy == NULL)
+	value_cpy = strdup(value);
+	if (value_cpy == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
