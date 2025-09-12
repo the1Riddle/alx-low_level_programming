14a9cc21585733b9b0c6b6aafa40316b87975e62
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2025-09-12 16:51:59 +0300

    Refactor insert_nodeint_at_index function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,35 +11,40 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	 unsigned int a;
-	listint_t *new;
-	listint_t *temp = *head;
+    unsigned int a;
+    listint_t *new;
+    listint_t *temp;
 
-	new = malloc(sizeof(listint_t));
-	if (!new || !head)
-		return (NULL);
+    if (head == NULL)
+        return (NULL);
 
-	new->n = n;
-	new->next = NULL;
+    temp = *head;
 
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
+    new = malloc(sizeof(listint_t));
+    if (!new)
+        return (NULL);
 
-	for (a = 0; temp && a < idx; a++)
-	{
-		if (a == idx - 1)
-		{
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
-		}
-		else
-			temp = temp->next;
-	}
+    new->n = n;
+    new->next = NULL;
 
-	return (NULL);
+    if (idx == 0)
+    {
+        new->next = *head;
+        *head = new;
+        return (new);
+    }
+
+    for (a = 0; temp && a < idx; a++)
+    {
+        if (a == idx - 1)
+        {
+            new->next = temp->next;
+            temp->next = new;
+            return (new);
+        }
+        else
+            temp = temp->next;
+    }
+
+    return (NULL);
 }
